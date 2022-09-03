9dbc72f4b8ad05d6376532b953de349ed89fe26e
Takumi Nagashima <emperte2008@gmail.com>
2022-09-03 22:25:00 +0900

    componentsで分けた

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,63 +1,25 @@
 <template>
-  <draggable v-model="lists" :options="{group: 'lists'}" class="row dragArea" @end="listMoved">
-    <div v-for="(list, index) in lists" class="col-3">
-      <h6>{{ list.name }}</h6>
-      <hr />
-
-      <draggable v-model="list.cards" :options="{group: 'cards'}" class="dragArea" @change="cardMoved">
-        <div v-for="(card, index) in list.cards" class="card card-body mb-3">
-          {{ card.name }}
-        </div>
-      </draggable>
-
-      <div class="card card-body">
-        <textarea v-model="messages[list.id]" class="form-control" ></textarea>
-        <button v-on:click="submitMessages(list.id)" class="btn btn-secondary">Add</button>
-      </div>
-    </div>
+  <draggable v-model="lists" :options="{group: 'lists'}" class="board dragArea" @end="listMoved">
+    <list v-for="(list, index) in lists" :list="list"></list>
   </draggable>
 </template>
 
 <script>
 import draggable from 'vuedraggable'
+import list from 'components/list'
 
 export default {
-  components: { draggable },
+  components: { draggable, list },
 
   props:["original_lists"],
 
   data: function(){
     return{
-      messages: {},
       lists: this.original_lists,
     }
   },
 
-  methods:{
-    cardMoved: function(event) {
-      const evt = event.added || event.moved
-      if (evt == undefined){ return }
-
-      const element = evt.element
-
-      const list_index = this.lists.findIndex((list) => {
-        return list.cards.find((card) => {
-          return card.id == element.id
-        })
-      })
-
-      var data = new FormData
-      data.append("card[list_id]", this.lists[list_index].id)
-      data.append("card[position]", evt.newIndex + 1)
-
-        Rails.ajax({
-        url: `/cards/${element.id}/move`,
-        type: "PATCH",
-        data: data,
-        dataType: "json",
-      })
-    },
-
+  methods: {
     listMoved: function(event) {
       var data = new FormData
       data.append("list[position]", event.newIndex + 1)
@@ -69,29 +31,15 @@ export default {
         dataType: "json",
       })
     },
-    submitMessages: function(list_id){
-      var data = new FormData
-      data.append("card[list_id]", list_id)
-      data.append("card[name]", this.messages[list_id])
-
-      Rails.ajax({
-        url: "/cards",
-        type: "POST",
-        data: data,
-        dataType: "json",
-        success: (data) => {
-          const index = this.lists.findIndex(item => item.id == list_id)
-          this.lists[index].cards.push(data)
-          this.messages[list_id] = undefined
-        }
-      })
-    }
   }
 }
 </script>
 
 <style scoped>
-.dragArea{
-  min-height: 20px;
+.board{
+  overflow-x: auto;
+  white-space: nowrap;
 }
+
+
 </style>
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -18,15 +18,17 @@ import TurbolinksAdapter from 'vue-turbolinks'
 Vue.use(Vuex)
 Vue.use(TurbolinksAdapter)
 
+window.store = {}
+
 
 document.addEventListener("turbolinks:load", function() {
     var element = document.querySelector("#boards")
-    if (element != undefined) {  
+    if (element != undefined) {
+        window.store.lists = JSON.parse(element.dataset.lists)
+        
         const app = new Vue({
             el: element,
-            data: {
-                lists: JSON.parse(element.dataset.lists)
-            },
+            data: window.store,
             template: "<App :original_lists='lists' />",
             components: { App }
         })
